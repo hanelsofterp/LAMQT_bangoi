cb3278f71794692a6e4a3926236fdcb923805d5f
lamqt <lamqt@hanelsoft.vn>
2016-10-13 11:39:23 +0700

    fix

--- purchase_receipt_other_rate_currency/__openerp__.py
+++ purchase_receipt_other_rate_currency/__openerp__.py
@@ -1,6 +1,9 @@
 {
     'name': 'Purchase Receipt for other currency',
-    'summary': 'Purchase Receipt for other currency',
+	'summary': """Purchase Receipt for other currency with record exchange rate
+					+ Allow to choose currency from header of the form
+                    + Allow to choose Exchange rate from header of the form
+                    + When system post journal entries: Need to get Currency, Currency Amount and convert to base currency (using the Exchange Rate on Purchase Receipt)""",
     'version': '1.0',
     'category': 'Purchase',
     'description': """ 
